a23a7bc5e58e1267e06bf63e9331025c114a70cb
watanabe <iamogroman@gmail.com>
2022-03-21 21:45:26 +0900

    log-in Sign-in done

--- src/views/form.vue
+++ src/views/form.vue
@@ -1,11 +1,16 @@
 <template>
   <form>
-    <br>
-    <br>
-    <br>
-    <br>
+    <br />
+    <br />
+    <br />
+    <br />
     <div class="container mt-5 pb-5 bg-light shadow-lg">
-      <div class="row pt-4 pb-3 bg-success text-white"><h1>シフトを選択してください</h1></div>
+      <div class="row pt-4 pb-3 bg-success text-white">
+        <h1>シフトを選択してください</h1>
+      </div>
+      <!-- <div class="row pt-4 pb-3 bg-success text-white" v-else>
+        <h1>送信完了しました！</h1>
+      </div> -->
       <div class="row mt-2">
         <div class="col mt-5"><p>月</p></div>
         <div class="col mt-5"><p>火</p></div>
@@ -18,12 +23,24 @@
       <div class="row">
         <div class="col form-group">
           <select
-            class="form-select"
+            class="form-select form-control"
             v-model="monday"
             aria-label="Default select example"
+            multiple
           >
-            <option value="true">ok</option>
-            <option value="false">無理</option>
+            <option value="1">10-11</option>
+            <option value="2">11-12</option>
+            <option value="3">12-13</option>
+            <option value="4">13-14</option>
+            <option value="5">14-15</option>
+            <option value="6">15-16</option>
+            <option value="7">16-17</option>
+            <option value="8">17-18</option>
+            <option value="9">18-19</option>
+            <option value="10">19-20</option>
+            <option value="11">20-21</option>
+            <option value="12">21-22</option>
+            <option value="13">22-23</option>
           </select>
         </div>
         <div class="col form-group">
@@ -31,10 +48,21 @@
             class="form-select"
             v-model="tuesday"
             aria-label="Default select example"
+            multiple
           >
-            <option selected></option>
-            <option value="true">ok</option>
-            <option value="false">無理</option>
+            <option value="1">10-11</option>
+            <option value="2">11-12</option>
+            <option value="3">12-13</option>
+            <option value="4">13-14</option>
+            <option value="5">14-15</option>
+            <option value="6">15-16</option>
+            <option value="7">16-17</option>
+            <option value="8">17-18</option>
+            <option value="9">18-19</option>
+            <option value="10">19-20</option>
+            <option value="11">20-21</option>
+            <option value="12">21-22</option>
+            <option value="13">22-23</option>
           </select>
         </div>
         <div class="col form-group">
@@ -42,9 +70,21 @@
             class="form-select"
             v-model="wednesday"
             aria-label="Default select example"
+            multiple
           >
-            <option value="true">ok</option>
-            <option value="false">無理</option>
+            <option value="1">10-11</option>
+            <option value="2">11-12</option>
+            <option value="3">12-13</option>
+            <option value="4">13-14</option>
+            <option value="5">14-15</option>
+            <option value="6">15-16</option>
+            <option value="7">16-17</option>
+            <option value="8">17-18</option>
+            <option value="9">18-19</option>
+            <option value="10">19-20</option>
+            <option value="11">20-21</option>
+            <option value="12">21-22</option>
+            <option value="13">22-23</option>
           </select>
         </div>
         <div class="col form-group">
@@ -52,9 +92,21 @@
             class="form-select"
             v-model="thursday"
             aria-label="Default select example"
+            multiple
           >
-            <option value="true">ok</option>
-            <option value="false">無理</option>
+            <option value="1">10-11</option>
+            <option value="2">11-12</option>
+            <option value="3">12-13</option>
+            <option value="4">13-14</option>
+            <option value="5">14-15</option>
+            <option value="6">15-16</option>
+            <option value="7">16-17</option>
+            <option value="8">17-18</option>
+            <option value="9">18-19</option>
+            <option value="10">19-20</option>
+            <option value="11">20-21</option>
+            <option value="12">21-22</option>
+            <option value="13">22-23</option>
           </select>
         </div>
         <div class="col form-group">
@@ -62,9 +114,21 @@
             class="form-select"
             v-model="friday"
             aria-label="Default select example"
+            multiple
           >
-            <option value="true">ok</option>
-            <option value="false">無理</option>
+            <option value="1">10-11</option>
+            <option value="2">11-12</option>
+            <option value="3">12-13</option>
+            <option value="4">13-14</option>
+            <option value="5">14-15</option>
+            <option value="6">15-16</option>
+            <option value="7">16-17</option>
+            <option value="8">17-18</option>
+            <option value="9">18-19</option>
+            <option value="10">19-20</option>
+            <option value="11">20-21</option>
+            <option value="12">21-22</option>
+            <option value="13">22-23</option>
           </select>
         </div>
         <div class="col form-group">
@@ -72,10 +136,21 @@
             class="form-select"
             v-model="saturday"
             aria-label="Default select example"
+            multiple
           >
-            <option selected></option>
-            <option value="true">ok</option>
-            <option value="false">無理</option>
+            <option value="1">10-11</option>
+            <option value="2">11-12</option>
+            <option value="3">12-13</option>
+            <option value="4">13-14</option>
+            <option value="5">14-15</option>
+            <option value="6">15-16</option>
+            <option value="7">16-17</option>
+            <option value="8">17-18</option>
+            <option value="9">18-19</option>
+            <option value="10">19-20</option>
+            <option value="11">20-21</option>
+            <option value="12">21-22</option>
+            <option value="13">22-23</option>
           </select>
         </div>
         <div class="col form-group">
@@ -83,9 +158,21 @@
             class="form-select"
             v-model="sunday"
             aria-label="Default select example"
+            multiple
           >
-            <option value="true">ok</option>
-            <option value="false">無理</option>
+            <option value="1">10-11</option>
+            <option value="2">11-12</option>
+            <option value="3">12-13</option>
+            <option value="4">13-14</option>
+            <option value="5">14-15</option>
+            <option value="6">15-16</option>
+            <option value="7">16-17</option>
+            <option value="8">17-18</option>
+            <option value="9">18-19</option>
+            <option value="10">19-20</option>
+            <option value="11">20-21</option>
+            <option value="12">21-22</option>
+            <option value="13">22-23</option>
           </select>
         </div>
       </div>
@@ -97,6 +184,7 @@
         </div>
       </div>
     </div>
+    {{ getData }}
   </form>
 </template>
 
@@ -124,10 +212,18 @@ export default {
       friday: "",
       saturday: "",
       sunday: "",
+      // seen: true,
     }
   },
   methods: {
     postSchedule() {
+      this.getAnswer()
+      addDoc(collection(db, "schedule"), {
+        schedule: this.schedules2,
+      })
+      // this.seen = !this.seen
+    },
+    getAnswer() {
       this.schedules2[0].monday = this.monday
       this.schedules2[1].tuesday = this.tuesday
       this.schedules2[2].wednesday = this.wednesday
@@ -135,9 +231,12 @@ export default {
       this.schedules2[4].friday = this.friday
       this.schedules2[5].saturday = this.saturday
       this.schedules2[6].sunday = this.sunday
-      addDoc(collection(db, "schedule"), {
-        schedule: this.schedules2,
-      })
+    },
+  },
+  computed: {
+    getData: function () {
+      this.getAnswer()
+      return this.schedules2
     },
   },
 }
--- src/views/sign-up.vue
+++ src/views/sign-up.vue
@@ -1,31 +1,102 @@
 <template>
-  <h1>Sign Up</h1>
-  <div class="sign-up">
-    <div class="email">
-      <input type="text" placeholder="email" name="uname" required /><br />
+  <div class="container p-2">
+    <div class="row"><h1>Sign Up</h1></div>
+    <div class="row">
+      <div class="container m-1 p-1">
+        <div class="row">e-mail</div>
+        <div class="row m-2">
+          <input type="text" v-model="email" />
+        </div>
+        <div class="row">password</div>
+        <div class="row m-2">
+          <input type="text" v-model="password" />
+        </div>
+        <div class="row">name</div>
+        <div class="row m-2">
+          <input type="text" v-model="employeeName" />
+        </div>
+        <div class="row">役職</div>
+        <div class="row m-2">
+          <select v-model="value" multiple>
+            <option value="true">ホール</option>
+            <option value="true">キッチン</option>
+          </select>
+        </div>
+      </div>
     </div>
-
-    <div class="pass">
-      <input type="password" placeholder="Password" name="psw" required /><br />
-    </div>
-    <div class="submit">
-      <button type="submit">Sign Up</button>
+    <div class="row">
+      <div class="col">
+        <button class="btn btn-primary btn-sm" v-on:click="signIn">登録</button>
+      </div>
     </div>
   </div>
+  {{ errorCode }},{{ errorMessage }}
+  <!-- {{ unko }} -->
 </template>
 
-<style>
-.sign-up{
-  text-align: center;
-  background-color: #101010;
-  height: 200px;
-  width: 200px;
-  padding-top: 20px;
-  border-radius: 15px;
-  margin: 0 auto;
-}
+<script>
+import { collection, addDoc, getDocs } from "firebase/firestore"
+// firebase.js で db として export したものを import
+import { db } from "../firebase"
 
-.pass {
-  margin: 50px 0;
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth"
+
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+      employeeName: "",
+      value: { ホール: "false", キッチン: "false" },
+      usersId: "",
+      unko: [],
+      errorCode: "",
+      errorMessage: "",
+      user: "",
+    }
+  },
+  methods: {
+    submit() {
+      addDoc(collection(db, "users"), {
+        uid: this.email,
+        password: this.password,
+        name: this.employeeName,
+        status: this.value,
+      })
+    },
+    signIn() {
+      const auth = getAuth()
+      createUserWithEmailAndPassword(auth, this.email, this.password)
+        .then((userCredential) => {
+          this.user = userCredential.user
+        })
+        .catch((error) => {
+          this.errorCode = error.code
+          this.errorMessage = error.message
+        })
+    },
+    logIn() {
+      const auth = getAuth()
+      signInWithEmailAndPassword(auth, this.email, this.password)
+        .then((userCredential) => {
+          this.user = userCredential.user
+        })
+        .catch((error) => {
+          this.errorCode = error.code
+          this.errorMessage = error.message
+        })
+    },
+  },
+  created: function () {
+    getDocs(collection(db, "users")).then((snapshot) => {
+      snapshot.forEach((doc) => {
+        this.unko.push(doc.data())
+      })
+    })
+  },
 }
-</style>
+</script>
