830c5e7f1c99bf8ca0708e956187a8100dc702b2
watanabe <iamogroman@gmail.com>
2022-03-27 22:31:38 +0900

    コンフリクト修正

--- src/components/friday.vue
+++ src/components/friday.vue
@@ -205,7 +205,7 @@
   </div>
 </template>
 <script>
-import { addDoc, collection, getDocs } from "firebase/firestore"
+import { collection, getDocs, addDoc } from "firebase/firestore"
 import { onAuthStateChanged, getAuth } from "firebase/auth"
 import { db } from "../firebase"
 
